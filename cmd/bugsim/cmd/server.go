51ef50bc02614627df018e0066952d3fcd44df4b
Lutz Behnke <lutz.behnke@gmx.de>
2022-03-28 23:14:13 +0200

    add templating for html, html content type header, try to parse pod id from request URL

--- cmd/bugsim/cmd/server.go
+++ cmd/bugsim/cmd/server.go
@@ -6,13 +6,16 @@ package cmd
 
 import (
 	"fmt"
+	"html/template"
 
 	"io"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/gorilla/mux"
 	"github.com/spf13/cobra"
+	v1 "k8s.io/api/core/v1"
 
 	"github.com/cypherfox/cloud-native-demo/pkg/k8s"
 )
@@ -21,6 +24,20 @@ var Port int16
 var k8sClient *k8s.K8sClient
 var Namespace string
 var Deployment string
+
+var templ *template.Template
+
+const root_tpl = `
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Welcome to BugSim</title>
+	</head>
+	<body>
+		{{range .Items}}<div>Pod: <a href="/api/delete/{{ .Name }}">{{ .Name }}</a></div>{{else}}<div><strong>no pods</strong></div>{{end}}
+	</body>
+</html>`
 
 // serverCmd represents the server command
 var serverCmd = &cobra.Command{
@@ -57,8 +74,15 @@ func doServer() error {
 		os.Exit(1)
 	}
 
+	templ, err = template.New("rootPage").Parse(root_tpl)
+	if err != nil {
+		fmt.Printf("Initializing root template failed: %s", err.Error())
+		return err
+	}
+
 	fmt.Println("Setting up pages")
-	http.HandleFunc("/", rootPage)
+	http.HandleFunc("/api/delete/{id}", deleteSinglePod)
+	http.HandleFunc("/", rootPage2)
 
 	fmt.Printf("Starting to serve on port %d\n", Port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", Port), nil))
@@ -67,6 +91,7 @@ func doServer() error {
 }
 
 func rootPage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	err := respPrintf(w, "Hello, Bug from %s \n", r.RemoteAddr)
 	if err != nil {
 		return
@@ -85,12 +110,51 @@ func rootPage(w http.ResponseWriter, r *http.Request) {
 
 	for i, pod := range pods.Items {
 
-		err = respPrintf(w, "Pod %d: %s\n", i, pod.Name)
+		err = printLink(w, i, pod.Name)
 		if err != nil {
 			return
 		}
 
 	}
+}
+
+func rootPage2(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	err := respPrintf(w, "Hello, Bug from %s \n", r.RemoteAddr)
+	if err != nil {
+		return
+	}
+
+	pods, err := k8sClient.GetPods(Namespace, Deployment)
+	if err != nil {
+		fmt.Printf("reading pods failed: %s\n", err.Error())
+		return
+	}
+
+	data := struct {
+		Items []v1.Pod
+	}{
+		Items: pods.Items,
+	}
+
+	err = templ.Execute(w, data)
+	if err != nil {
+		fmt.Printf("generating root page from template failed: %s\n", err.Error())
+		return
+	}
+
+}
+
+func printLink(w http.ResponseWriter, num int, podName string) error {
+	respPrintf(w, "Pod %d: <a href=\"/api/delete/%s\">%s</a>\n", num, podName, podName)
+	return nil
+}
+
+func deleteSinglePod(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	fmt.Printf("Key: %s\n", key)
 }
 
 func respPrintf(w http.ResponseWriter, format string, a ...interface{}) error {
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/cypherfox/cloud-native-demo
 go 1.16
 
 require (
+	github.com/gorilla/mux v1.8.0
 	github.com/spf13/cobra v1.4.0
 	k8s.io/api v0.19.16
 	k8s.io/apimachinery v0.19.16
